f56ee51b44501dee3daaa3ebc1f45ad6b74fe3b3
Clickadelic <toby.hopp@gmail.com>
2025-08-08 11:39:32 +0200

    form

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -4,6 +4,7 @@ if(!isset($_SESSION['user'])) { ?>
     <!-- Logged out -->
     <h2>Willkommen zurück bei den Snippets</h2>
     <p>Eine kleine Snippetdatenbank für Webentwickler.</p>
+    <?php include __DIR__ .'/templates/forms/login-form.tpl.php'; ?>
 <?php } else { ?>
     <!-- Logged in -->
     <h2>Willkommen zurück, <?= $_SESSION['user']->getUsername(); ?>!</h2>
